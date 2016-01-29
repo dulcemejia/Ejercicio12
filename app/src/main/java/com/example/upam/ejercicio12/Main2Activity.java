41cd9c1bcdc3701b7c171e9a5c3577c2a4206e30
NanoCodeInc <nanocode.inc@gmail.com>
2016-01-29 12:53:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/upam/ejercicio12/Main2Activity.java
@@ -0,0 +1,42 @@
+package com.example.upam.ejercicio12;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private WebView webView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        webView1=(WebView) findViewById(R.id.webView);
+
+        Bundle bundle=getIntent().getExtras();
+        String dato=bundle.getString("url");
+        webView1.loadUrl("http://"+dato);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void salir(View v){
+        finish();
+    }
+
+}
